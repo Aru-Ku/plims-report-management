ce770ce497b0734f74f3e972c7013e5dbf1ace87
mastrero <arunaiekhil@gmail.com>
2020-09-07 16:04:04 +0530

    CLOSED #1 - ADD Custom Table for viewing Forms - ADD Loader UI Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import "./utils/firebase";
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
 import NewForm from "./components/NewForm";
 import FormSubmitted from "./components/FormSubmitted";
 import Requisitions from "./components/Requisitions";
+import DisplayForm from "./components/DisplayForm";
 
 //* Routes Should be Changed
 export default function App() {
@@ -16,8 +17,10 @@ export default function App() {
 				<Switch>
 					<Route exact path='/new' component={NewForm} />
 					<Route exact path='/submitted' component={FormSubmitted} />
+					<Route exact path='/forms/:form_id' component={DisplayForm} />
 					<Route exact path='/forms' component={Requisitions} />
 					<Route exact path='/' component={LandingPage} />
+					<Redirect to='/' />
 				</Switch>
 			</div>
 		</Fragment>
---
+++ src/utils/formsTable.js
@@ -0,0 +1,27 @@
+export const headings = [
+	{
+		columnId: "id",
+		columnHead: "Form ID",
+		element: "link",
+	},
+	{
+		columnId: "submittedTime",
+		columnHead: "Submitted Time",
+		element: "time",
+	},
+	{
+		columnId: "status",
+		columnHead: "Status",
+		element: "badge",
+	},
+	{
+		columnId: "physician",
+		columnHead: "Physician",
+		element: "name",
+	},
+	{
+		columnId: "reviewedBy",
+		columnHead: "Reviewed By",
+		element: "name",
+	},
+];
